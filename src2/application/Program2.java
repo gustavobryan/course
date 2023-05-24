2cf73e0d9e0d96c3c288ed7e53b2b57c535752c6
Gustavo Bryan <devgustavob@gmail.com>
2023-05-24 11:16:11 -0300

    toString

--- src2/application/Program2.java
+++ src2/application/Program2.java
@@ -21,7 +21,7 @@ public class Program2 {
 		System.out.print("Quantity in stock: ");
 		product.quantity = sc.nextInt();
 		
-		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+		System.out.println(product);		
 		
 		sc.close();
 	}
